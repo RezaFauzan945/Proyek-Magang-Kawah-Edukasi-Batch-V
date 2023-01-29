b7fe2260623568a0fff52172ccdd16596cf65ac9
RezaFauzan945 <rezafauzan945@gmail.com>
2023-01-29 18:38:29 +0700

    Merubah cara input Bilangan yang difaktor kan melalui query param

--- tugas/tugas-ke-1/src/main/java/id/rezafauzan/controllers/FactorialResource.java
+++ tugas/tugas-ke-1/src/main/java/id/rezafauzan/controllers/FactorialResource.java
@@ -3,16 +3,16 @@ package id.rezafauzan.controllers;
 import javax.transaction.Transactional;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
 
 @Path("/factorial")
 public class FactorialResource {
 
     @GET
     @Transactional
-    public int factorial()
+    public int factorial(@QueryParam("bilangan") int bilangan )
     {
         int factorial = 1;
-        int bilangan =  6;
         for (int i = 1; i <= bilangan; i++) {
             factorial *= i;
         }
